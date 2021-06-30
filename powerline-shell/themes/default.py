187a1a7bc57bf75e8fd9de6f00d7ffd8c75600b5
Lê Công Minh Khôi <lcngmnhkhoi@gmail.com>
2021-06-30 07:40:13 +0700

    Update default.py

--- powerline-shell/themes/default.py
+++ powerline-shell/themes/default.py
@@ -4,16 +4,16 @@ from powerline_shell.themes.default import DefaultColor
 class Color(DefaultColor):
     """Basic theme which only uses colors in 0-15 range"""
     USERNAME_FG = 232
-    USERNAME_BG = 166
+    USERNAME_BG = 201
     USERNAME_ROOT_BG = 1
 
     HOSTNAME_FG = 8
     HOSTNAME_BG = 7
 
     HOME_SPECIAL_DISPLAY = False
-    PATH_BG = 236  
-    PATH_FG = 208  
-    CWD_FG = 214  # white
+    PATH_BG = 236
+    PATH_FG = 207
+    CWD_FG = 213
     SEPARATOR_FG = 27
 
     READONLY_BG = 1
@@ -24,11 +24,11 @@ class Color(DefaultColor):
     REPO_DIRTY_BG = 1   # red
     REPO_DIRTY_FG = 15  # white
 
-    JOBS_FG = 220
+    JOBS_FG = 197
     JOBS_BG = 237
 
     CMD_PASSED_BG = 238
-    CMD_PASSED_FG = 226
+    CMD_PASSED_FG = 219
     CMD_FAILED_BG = 11
     CMD_FAILED_FG = 0
 
